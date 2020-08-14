03608e882c3d2444974f4d379729bcb1645dc6a3
kennethcdeguzman <kennethcdeguzman@yahoo.com>
2020-08-14 12:56:40 +0800

    Unit Test

---
+++ calculator/src/test/java/com/kenneth/calculator/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.kenneth.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void TestSum() {
+        assertEquals(5, calculator.sum(2,3));
+    }
+}
