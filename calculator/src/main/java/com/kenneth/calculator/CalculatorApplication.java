aef7c1ab18edbae6700fab57ea5e643d8a4e4b05
U028KXD <kennetharnold.deguzman@carlsonwagonlit.com>
2020-08-14 12:17:54 +0800

    code enhancement

--- calculator/src/main/java/com/kenneth/calculator/CalculatorApplication.java
+++ calculator/src/main/java/com/kenneth/calculator/CalculatorApplication.java
@@ -2,17 +2,12 @@ package com.kenneth.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
-	}
-    @Service
-    public class calculator {
-		int sum(int a, int b){
-			return a+b;
-		}
 	}
 }
--- calculator/src/main/java/com/kenneth/calculator/CalculatorController.java
+++ calculator/src/main/java/com/kenneth/calculator/CalculatorController.java
@@ -1,3 +1,11 @@
+package com.kenneth.calculator;
+
+import com.kenneth.calculator.Calculator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 class CalculatorController {
     @Autowired
